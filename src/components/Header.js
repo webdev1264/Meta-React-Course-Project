837e55a33d6bcb2d185741244f3702d0b6f4c4ea
webdev <webdev1264@gmail.com>
2024-03-28 18:37:42 +0100

    Header layout and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import {
@@ -43,10 +42,20 @@ const Header = () => {
       });
     }
   };
+  [];
+
+  const socialsToRender = socials.map((social, i) => {
+    return (
+      <a key={i} href={social.url}>
+        <FontAwesomeIcon icon={social.icon} size="2x" />
+      </a>
+    );
+  });
 
   return (
     <Box
       position="fixed"
+      zIndex={100}
       top={0}
       left={0}
       right={0}
@@ -54,21 +63,20 @@ const Header = () => {
       transitionProperty="transform"
       transitionDuration=".3s"
       transitionTimingFunction="ease-in-out"
-      backgroundColor="#18181b"
-    >
+      backgroundColor="#18181b">
       <Box color="white" maxWidth="1280px" margin="0 auto">
-        <HStack
-          px={16}
-          py={4}
-          justifyContent="space-between"
-          alignItems="center"
-        >
+        <HStack px={16} py={4} justifyContent="space-between" alignItems="center">
           <nav>
-            {/* Add social media links based on the `socials` data */}
+            <HStack spacing={8}>{socialsToRender}</HStack>
           </nav>
           <nav>
             <HStack spacing={8}>
-              {/* Add links to Projects and Contact me section */}
+              <a href="#projects" onClick={handleClick("projects")}>
+                Projects
+              </a>
+              <a href="#contact-me" onClick={handleClick("contact-me")}>
+                Contact me
+              </a>
             </HStack>
           </nav>
         </HStack>
