fbd1e2eb7631f68b5183d16d4429a74a635e5038
webdev <webdev1264@gmail.com>
2024-03-28 18:38:14 +0100

    Landing section layout and styling

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -4,7 +4,7 @@ import FullScreenSection from "./FullScreenSection";
 
 const greeting = "Hello, I am Pete!";
 const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const bio2 = "specialized in React";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
@@ -13,9 +13,17 @@ const LandingSection = () => (
     justifyContent="center"
     alignItems="center"
     isDarkBackground
-    backgroundColor="#2A4365"
-  >
-
+    backgroundColor="#2A4365">
+    <VStack spacing={10}>
+      <VStack spacing={2}>
+        <Avatar size="xl" src="https://i.pravatar.cc/150?img=7" name="Pete" />
+        <Heading size="xs">{greeting}</Heading>
+      </VStack>
+      <VStack spacing={2}>
+        <Heading size="2xl">{bio1}</Heading>
+        <Heading size="2xl">{bio2}</Heading>
+      </VStack>
+    </VStack>
   </FullScreenSection>
 );
 
